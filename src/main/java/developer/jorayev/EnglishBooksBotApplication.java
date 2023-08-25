91f0a5e2b39b730c6b2aa7a1810a88272f1b2a70
developer.jorayev <jorayevdev002@gmail.com>
2023-08-25 21:26:23 +0500

    first commit

---
+++ src/main/java/developer/jorayev/EnglishBooksBotApplication.java
@@ -0,0 +1,13 @@
+package developer.jorayev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishBooksBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishBooksBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/developer/jorayev/EnglishBooksBotApplicationTests.java
@@ -0,0 +1,13 @@
+package developer.jorayev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishBooksBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
